56793a4d6ee94b2b5bb723482e97122da6f2d695
yumnamurad <128488229+Yuy29@users.noreply.github.com>
2023-04-04 21:54:29 +0200

    Add files via upload

---
+++ assign_01_problem_01_code.cpp.cpp
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+//yumna murad20216333
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin>>str;
+    cout<<"Hello, "<<str;
+}
---
+++ assign_01_problem_03_code.cpp.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    long long y;
+    char ch;
+    float f1;
+    double d;
+    
+    cin>> x >> y >> ch >> f1 >> d ;
+    cout<< x <<"\n" << y <<"\n" << ch <<"\n" << f1<<"\n"<<d<< "\n";
+    return 0;
+}
